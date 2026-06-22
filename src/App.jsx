57d0c29039e626d4a3a9c5b3030c13b4ac656fac
Aagam-07-04-08 <co2024.aagam.shah@ves.ac.in>
2026-06-22 17:06:47 +0530

    Event Handle

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,10 @@
 import { useState } from "react"
+import Person from "./Person.jsx"
 
 function App() {
-    
-  const [person, setPerson] = useState({
-        name: "Aagam",
-        age: 18
-    });
-
-  function updatePerson(obj) {
-     return {
-        ...obj,
-        name: "Rahul",
-        age: obj.age + 1
-      };
-    }
-
-
-  function handleClick() {
-      const updated = updatePerson(person);
-      setPerson(updated);
-    }
 
   return (
-    <div>
-      <h2>Person Details</h2>
-      <p>Name: {person.name}</p>
-      <p>Age: {person.age}</p>
-      <button onClick={handleClick}>Update Details</button>
-    </div>
+    <Person/>
   )
 }
 
---
+++ src/Person.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+function person(){
+    
+    const [person,setPerson] = useState(
+        {
+            name:"Guest",
+            age:0
+        }
+    )
+
+    const [newName,setNewName] = useState("");
+    const [newAge,setNewAge] = useState(0);
+
+        function handleClick(){
+            setPerson(
+                {
+                    ...person,
+                    name: newName !== "" ? newName : person.name,
+                    age: newAge !== "" ? newAge : person.age
+                }
+            )
+        }
+
+    return(
+        <div className="container">
+            <div className="person">
+            <h2>Person Details</h2>
+            <p>Name: {person.name}</p>
+            <p>Age: {person.age}</p>
+            <label htmlFor="user">UserName: </label>
+            <input type="text" onChange={(e) => setNewName(e.target.value)} id="user" placeholder="enter username"/>
+            <br />
+            <label htmlFor="age">Age: </label>
+            <input type="number" onChange={(e) => setNewAge(e.target.value)} placeholder="enter age" id="age" />
+            <br />
+            <button onClick={handleClick}>Update Details</button>
+            </div>
+        </div>
+    )
+}
+
+export default person
